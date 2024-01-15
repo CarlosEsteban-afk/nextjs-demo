3538f1139e61f0a12196004fcea2ea57c6627611
CarlosEsteban2122 <c.pena17@ufromail.cl>
2024-01-15 17:49:32 -0300

    google analytics example

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -8,7 +9,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-WRZ1P8RSZY"></Script>
+      <Script strategy='afterInteractive' id='google-analytics'>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WRZ1P8RSZY');`}
+      </Script>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
+/**
+ * <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WRZ1P8RSZY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WRZ1P8RSZY');
+</script>
+ */
