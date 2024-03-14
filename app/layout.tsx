dadc8ab0af4c814d030fa419b1655ad0f9ffb2c8
CarlosEsteban2122 <c.pena17@ufromail.cl>
2024-03-14 01:58:52 -0300

    Guardando antes de formatear

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import Script from 'next/script';
-
+import { Providers } from "./providers";
 export default function RootLayout({
   children,
 }: {
@@ -9,10 +9,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
       <Script strategy='afterInteractive' src="https://www.googletagmanager.com/gtag/js?id=G-WRZ1P8RSZY"></Script>
       <Script strategy='afterInteractive' id='google-analytics'>
-    
+
         {`window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
